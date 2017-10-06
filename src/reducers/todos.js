0521ef6c011ebbd4af647bcd36abe6d1672bce3c
Chip Irek <chip.irek@gmail.com>
2017-10-06 12:43:36 -0400

    Added ability to insert record through API by handling a dispatch in superagent

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -1,41 +1,51 @@
 const todo = (state, action) => {
-	switch (action.type) {
-	case 'ADD_TODO':
-		return {
-			id: action.id,
-			title: action.title,
-			complete: false
-		}
-	case 'TOGGLE_TODO':
-		if (state.id !== action.id) {
-			return state
-		}
+  
+  switch (action.type) {
+  
+    case 'ADD_TODO':
+      console.log("in pt1")
+      let new_todo = {
+        id: action.id,
+        title: action.title,
+        complete: false
+      }
+      return new_todo
 
-		return Object.assign({}, state, {
-			complete: !state.complete
-		})
-	default:
-		return state
+    case 'TOGGLE_TODO':
+      if (state.id !== action.id) {
+        return state
+      }
+
+      return Object.assign({}, state, {
+        complete: !state.complete
+      })
+
+    default:
+		  return state
 	}
 }
 
 const todos = (state = [], action) => {
 	switch (action.type) {
-	case 'ADD_TODO':
+    
+  case 'ADD_TODO':
+    console.log("in pt2")    
 		return [
 			...state,
 			todo(undefined, Object.assign(action, {
 				id: state.length
 			}))
 		]
-	case 'TOGGLE_TODO':
+  
+  case 'TOGGLE_TODO':
 		return state.map(t =>
 			todo(t, action)
 		)
-	case 'GET_TODO_DATA_RECEIVED':
+  
+  case 'GET_TODO_DATA_RECEIVED':
 		return action.data
   
-    default:
+  default:
 		return state
 	}
 }
